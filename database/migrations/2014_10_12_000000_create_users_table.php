7003d9df8738dd98ad44dcc627c8b4abac2f4488
Dylan Aben Alvarez <daben@bilky.es>
2018-04-21 10:25:21 +0200

    Create User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->integer('role_id');
+            $table->integer('is_active')->index()->unsigned()->nullable();
+            $table->string('name')->default(0);
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
